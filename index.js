8cadb242f8bdbe1e2e12ca7a8b24c13a4ba3252c
Jonathan Ong <jonathanrichardong@gmail.com>
2012-12-07 01:33:45 -0800

    whatever

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function I(x) {
 }
 
 var selfClosing = {
-  meta: 1,
-  img: 1,
-  link: 1,
-  input: 1,
-  source: 1,
-  area: 1,
-  base: 1,
-  col: 1,
-  br: 1,
-  hr: 1
+  meta: true,
+  img: true,
+  link: true,
+  input: true,
+  source: true,
+  area: true,
+  base: true,
+  col: true,
+  br: true,
+  hr: true
 }
 
 /*
@@ -27,9 +27,7 @@ function attributesToString(attributes) {
 
   Object.keys(attributes).forEach(function(attribute) {
     var value = attributes[attribute]
-    if (Array.isArray(value)) {
-      value = value.filter(I).join(' ')
-    }
+    if (Array.isArray(value)) value = value.filter(I).join(' ');
     if (!value) return;
 
     buf += ' ' + attribute
