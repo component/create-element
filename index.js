c9e772a0dfa707c953bfe6ba89c5631fc5e4a214
Jonathan Ong <jonathanrichardong@gmail.com>
2012-12-19 21:54:27 -0800

    more modular :D

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
-function I(x) {
-  return x
+function createElement(tagName, attributes, block) {
+  return createElement.element.call(this, tagName, attributes, block)
 }
 
-var selfClosing = {
-  meta: true,
-  img: true,
-  link: true,
-  input: true,
-  source: true,
-  area: true,
-  base: true,
-  col: true,
-  br: true,
-  hr: true
+module.exports = createElement
+
+/*
+
+  tagName [string]
+  attributes [object] (optional)
+  block [string || function] (optional)
+
+*/
+createElement.element = function (tagName, attributes, block) {
+  if (!block && attributes && Object(attributes) !== attributes) {
+    block = attributes
+    attributes = null
+  }
+
+  var buf = createElement.openingTag(tagName, attributes)
+
+  if (block) {
+    if (typeof block === 'function') {
+      buf += block.call(this, '')
+    } else {
+      buf += block
+    }
+  }
+
+  if (block || !createElement.selfClosingTags[tagName]) {
+    buf += createElement.closingTag(tagName)
+  }
+
+  return buf
+}
+
+createElement.openingTag = function (tagName, attributes) {
+  return '<' + tagName +
+    (attributes ? createElement.attributes(attributes) : '') +
+    '>'
+}
+
+createElement.closingTag = function (tagName) {
+  return '</' + tagName + '>'
 }
 
 /*
 
   attributes [object]
 
-    [string] : [string || array of strings || object of strings || boolean]
+    [string] : [string || array of strings || object of booleans || boolean]
+
+  result will have a leading space
 
 */
-function attributesToString(attributes) {
+createElement.attributes = function (attributes) {
   var buf = ''
 
-  Object.keys(attributes).forEach(function(attribute) {
+  Object.keys(attributes).forEach(function (attribute) {
     var value = attributes[attribute]
 
     if (!value) return;
 
     if (Array.isArray(value)) {
-      value = value.filter(I).join(' ')
+      value = value.filter(function (x) {
+        return x
+      }).join(' ')
     } else if (Object(value) === value) {
       value = Object.keys(value).filter(function (key) {
         return value[key]
@@ -47,27 +80,15 @@ function attributesToString(attributes) {
   return buf
 }
 
-/*
-
-  tagName [string]
-  attributes [object] (optional)
-  block [string || function] (optional)
-
-*/
-exports = module.exports = function(tagName, attributes, block) {
-  if (!block && attributes && Object(attributes) !== attributes) {
-    block = attributes
-    attributes = null
-  }
-
-  var buf = '<' + tagName
-  if (attributes) buf += attributesToString(attributes);
-  buf += '>'
-  if (block) buf += typeof block === 'function' ? block() : block;
-  if (block || !selfClosing[tagName]) buf += '</' + tagName + '>';
-  return buf
+createElement.selfClosingTags = {
+  meta: true,
+  img: true,
+  link: true,
+  input: true,
+  source: true,
+  area: true,
+  base: true,
+  col: true,
+  br: true,
+  hr: true
 }
-
-exports.selfClosing = selfClosing
-
-exports.attributesToString = attributesToString
